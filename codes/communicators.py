32ba7b1579cee533ae61a62604475a721f2b2aa7
lubos <lubos31262@gmail.com>
2021-12-06 12:10:32 +0100

    connection to offer-cache - in progress

--- codes/communicators.py
+++ codes/communicators.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 import redis
+import r2r_offer_utils.cache_operations
+import r2r_offer_utils.advanced_logger
+
 import configparser as cp
 from os import path
 
@@ -33,15 +36,34 @@ class AgreementLedgerCommunicator(Communicator):
 
 
 class OfferCacheCommunicator(Communicator):
-    def __init__(self, data, config):
+    def __init__(self):
         # main_module_name = path.splitext(path.basename(sys.modules["__main__"].__file__))[0]
         #
         # config = cp.ConfigParser()
         # config.read(f'{main_module_name}.conf')
-        if config is not None:
-            CACHE_HOST = config.get('cache', 'host')
-            CACHE_PORT = config.get('cache', 'port')
-            cache = redis.Redis(host=CACHE_HOST, port=CACHE_PORT, decode_responses=True)
+        if r2r_offer_utils.advanced_logger.config is not None:
+            CACHE_HOST = r2r_offer_utils.advanced_logger.config.get('cache', 'host')
+            CACHE_PORT = r2r_offer_utils.advanced_logger.config.get('cache', 'port')
+            cache = redis.Redis(host='localhost', port=CACHE_PORT, decode_responses=True)
+
+            try:
+                output_offer_level, output_tripleg_level = r2r_offer_utils.cache_operations.read_data_from_cache_wrapper(
+                    cache,
+                    "46a4fa0d-2fd2-4317-8441-1b6a9511f1e7",
+                    ["bookable_total", "complete_total"],
+                    ["duration", "can_share_cost"])
+
+                print("output_offer_level = ", output_offer_level)
+                print("output_tripleg_level = ", output_tripleg_level)
+            except redis.exceptions.ConnectionError as exc:
+                r2r_offer_utils.advanced_logger.logging.error("Reading from cache failed.")
+                #response.status_code = 424
+                return None
+
+
+
+
+
 
     def accesRuleData(self) -> bool:
         # if the request was successful

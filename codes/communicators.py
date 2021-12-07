d7ebdbe05957c3acd18cb05efb1702e0019e71be
lubos <lubos31262@gmail.com>
2021-12-07 11:29:17 +0100

    connection to offer-cache - first draft

--- codes/communicators.py
+++ codes/communicators.py
@@ -1,28 +1,36 @@
 from abc import ABC, abstractmethod
 import redis
 import r2r_offer_utils.cache_operations
-import r2r_offer_utils.advanced_logger
+import logging
 
 import configparser as cp
 from os import path
 
 
+logger = logging.getLogger('incentive_provider_api.communicators')
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 class Communicator(ABC):
     def __init__(self, data):
         self.data = data
 
     @abstractmethod
-    def accesRuleData(self) -> bool:
+    def accessRuleData(self) -> bool:
         pass
 
-
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 class AgreementLedgerCommunicator(Communicator):
     def __init__(self, data):
         super().__init__(data)
 
         # objects required for requests
 
-    def accesRuleData(self) -> bool:
+    def accessRuleData(self) -> bool:
         # if the request was successful extract the data
         self.authenticate()
         self.obtainRequest(self.data['url_suffix'][0], self.data['values'][0])
@@ -33,38 +41,52 @@ class AgreementLedgerCommunicator(Communicator):
 
     def obtainRequest(self, url_suffix, id):
         pass
-
-
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+"""
+class ensuring communication with the offer Cache
+"""
 class OfferCacheCommunicator(Communicator):
+
+    """
+        Establish connection with the offer cache.
+    """
     def __init__(self):
-        # main_module_name = path.splitext(path.basename(sys.modules["__main__"].__file__))[0]
-        #
-        # config = cp.ConfigParser()
-        # config.read(f'{main_module_name}.conf')
         if r2r_offer_utils.advanced_logger.config is not None:
-            CACHE_HOST = r2r_offer_utils.advanced_logger.config.get('cache', 'host')
-            CACHE_PORT = r2r_offer_utils.advanced_logger.config.get('cache', 'port')
-            cache = redis.Redis(host='localhost', port=CACHE_PORT, decode_responses=True)
+            # read connection parameters from the config file
+            CACHE_HOST      = r2r_offer_utils.advanced_logger.config.get('cache', 'host')
+            CACHE_PORT      = r2r_offer_utils.advanced_logger.config.get('cache', 'port')
 
+            logger.info(f"Connecting to offer cache: CACHE_HOST = {CACHE_HOST}, CACHE_PORT = {CACHE_PORT}")
             try:
-                output_offer_level, output_tripleg_level = r2r_offer_utils.cache_operations.read_data_from_cache_wrapper(
-                    cache,
-                    "46a4fa0d-2fd2-4317-8441-1b6a9511f1e7",
-                    ["bookable_total", "complete_total"],
-                    ["duration", "can_share_cost"])
-
-                print("output_offer_level = ", output_offer_level)
-                print("output_tripleg_level = ", output_tripleg_level)
+                # establish connection to the offer cache
+                self.cache           = redis.Redis(host=CACHE_HOST, port=CACHE_PORT, decode_responses=True)
             except redis.exceptions.ConnectionError as exc:
-                r2r_offer_utils.advanced_logger.logging.error("Reading from cache failed.")
-                #response.status_code = 424
+                logger.error("Connection to the offer cache has not been established.")
                 return None
+        else:
+            logger.error('Could not read config file in communicators.py')
+            return None
+
+    """
+        Request data from the offer cache.
+    """
+    def read_data_from_offer_cache(self, request_id, list_offer_level_keys, list_tripleg_level_keys):
+        try:
+            # read data from the offer cache
+            logger.info(f"Read data from the offer cache: request_id = {request_id}, list_offer_level_keys = {list_offer_level_keys}, list_tripleg_level_keys = {list_tripleg_level_keys}")
+            output_offer_level, output_tripleg_level = r2r_offer_utils.cache_operations.read_data_from_cache_wrapper(
+                self.cache,
+                request_id,
+                list_offer_level_keys,
+                list_tripleg_level_keys)
+        except redis.exceptions.ConnectionError as exc:
+            logger.error("Reading from the offer cache failed in communicators.py.")
+            return None
+        return {'output_offer_level':output_offer_level, 'output_tripleg_level':output_tripleg_level}
 
 
-
-
-
-
-    def accesRuleData(self) -> bool:
+    def accessRuleData(self) -> bool:
         # if the request was successful
         pass
--- incentive_provider_api.py
+++ incentive_provider_api.py
@@ -11,8 +11,11 @@ app = Flask(service_name)
 #
 # Example of the request
 #
-# curl -v -X GET "http://127.0.0.1:5002/incentive_provider/?request_id=76704113-6ad8-43d5-a7cc-55e48450232a"
+# curl -v -X GET "http://127.0.0.1:5003/incentive_provider/?request_id=46a4fa0d-2fd2-4317-8441-1b6a9511f1e7"
 
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 @app.route('/incentive_provider/', methods=['GET'])
 def return_incentives():
     args = request.args.to_dict()
@@ -20,13 +23,44 @@ def return_incentives():
     logger.info(f"GET request for incentives obtained with request_id = {request_id}")
 
     # test communicator
-    ALC = codes.communicators.OfferCacheCommunicator()
+    ALC                                         = codes.communicators.OfferCacheCommunicator()
 
+    dict                                        = ALC.read_data_from_offer_cache(request_id,
+                                                                                [],
+                                                                                ["duration",
+                                                                                 "cleanliness",
+                                                                                 "space_available",
+                                                                                 "ride_smoothness",
+                                                                                 "seating_quality",
+                                                                                 "internet_availability",
+                                                                                 "plugs_or_charging_points",
+                                                                                 "silence_area_presence",
+                                                                                 "privacy_level",
+                                                                                 "user_feedback",
+                                                                                 "bike_on_board",
+                                                                                 "likelihood_of_delays",
+                                                                                 "last_minute_changes",
+                                                                                 "frequency_of_service",
+                                                                                 "business_area_presence"])
+
+
+    '''
+    dict                                        = ALC.read_data_from_offer_cache(request_id,
+                                                                                  ["bookable_total", "complete_total"],
+                                                                                  ["duration", "can_share_cost"])
+    '''
+
+
+    print("dict = ", dict)
     return "{}", 200
-
-
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 if __name__ == '__main__':
     FLASK_PORT = config.get('flask', 'port')
     os.environ["FLASK_ENV"] = "development"
     app.run(port=int(FLASK_PORT), debug=True, use_reloader=False)
     exit(0)
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################

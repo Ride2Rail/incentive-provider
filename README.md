991fddf97f14868a85a22cbb777e9da54df8dabc
lubos <lubos31262@gmail.com>
2022-01-19 13:51:33 +0100

    docu and comments

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # Incentive provider
 
 This module pre-evaluates the eligibility to receive travel incentives. The incentives 
-are associated with travel offers. Current version supports the following incentives listed in the Table.
+are associated with travel offers. Current version supports incentives listed in the following Table:
 
 | Incentive | Description |
 |:---------:|-------------|
-|     10%Discount     | The incentive is associated with a travel offer if at least one ride-sharing leg is included in the travel offer.       |
-|     20%Discount     | The incentive is associated with a travel offer if the travel offer contains at least one ride-sharing leg and the passenger completed at least 3 ride-shares. The counter of completed ride-shares is set to zero after allocating the incentive.  |
-|      FreeSeat       | The incentive is associated with a travel offer if at least one other passenger booked at least one of the ride-shares included in the travel offer. |
+|     10%Discount     | The incentive is associated with a travel offer item if at least one ride-sharing leg is included in any travel offer item.       |
+|     20%Discount     | The incentive is associated with a travel offer item if the offer item contains at least one ride-sharing leg and the passenger completed at least 3 ride-shares. The counter of completed ride-shares is set to zero after allocating the incentive.  |
+|      FreeSeat       | The incentive is associated with a travel offer item if at least one other passenger booked at least one of the ride-shares included in the travel offer item. |
 
 ## Calculation of incentives
 
 ### Incentive 10%Discount
+For a given _request_id_, the corresponding list of travel offer items and the transport modes associated with 
+legs are obtained from the [offer-cache](https://github.com/Ride2Rail/offer-cache).
+Based on this data, the presence of ride sharing legs is analysed, in the method _checkFulfilled()_ of the class _RideSharingInvolved_ 
+implemented in the script [rules.py](https://github.com/Ride2Rail/incentive-provider/blob/main/codes/rules.py).
 
-For a given _request_id_ the corresponding list of travel offers and their and transport modes of 
-
-
-### Incentive 10%Discount
+### Incentive 20%Discount
+For a given _request_id_, the corresponding list of travel offer items, Traveler ID and the transport modes associated with 
+legs are obtained from the [offer-cache](https://github.com/Ride2Rail/offer-cache). 
+If an offer items contains a ridesharing leg, Traveler ID is used to requested the Agreement Ledger 
+module to provide infomration about the eligibility to receive 20%Discount incentive. Otherwise, it is concluded that
+offer item is concluded to be not eligible for this incentive. The evaluation is implemented by 
+the method checkFulfilled()_ of the class _ThreePreviousEpisodesRS_ 
+implemented in the script [rules.py](https://github.com/Ride2Rail/incentive-provider/blob/main/codes/rules.py).
 
 ### Incentive FreeSeat
 
 
-# implementation
+
+# Implementation
 
 # Usage
 
@@ -34,6 +43,15 @@ The module "Incentive provider" can be launched from the terminal locally by run
  * Debug mode: on
  * Running on http://127.0.0.1:5003/ (Press CTRL+C to quit)
 ```
+
+## Requesting Data provider service
+Example of the curl command to receive a JSON file with information about the incentives associate with the travel offers included in the request which is identified by  < request_id >:
+```bash
+curl -X GET "http://127.0.0.1:5003/incentive_provider/?request_id=< request_id >"
+```
+
+Example of the returned JSON file:
+
 
 ```JSON
 {
--- codes/rules.py
+++ codes/rules.py
@@ -127,66 +127,6 @@ class RideSharingInvolved(Rule):
             logger.error(f"Rule: RideSharingInvolved State: No data extracted from the Offer cache.")
             return None
 
-'''
-example of the output received from the offer cache - trip_offer_data content
-
-dict =  {'output_offer_level': {'offer_ids': ['cb32d4fe-47fd-4b2f-aefa-01251d2fe3c6', '7e6c9839-d276-4e9a-8a30-30b3ac1557b9', '8cd79ed6-73fd-41c6-beec-60c997f675a4', 'ad124a6c-8cce-4936-a054-97a1f67c60b7', 'fa24dca4-e68a-4f66-b9ff-7a8aad78a74f', 'ae7eff45-5df0-4e7b-bd4e-ec4dc1a79d2d', 'e945ec09-f74c-42ed-8ae6-809945df54de'], 
-
-'cb32d4fe-47fd-4b2f-aefa-01251d2fe3c6': {},
-'7e6c9839-d276-4e9a-8a30-30b3ac1557b9': {}, 
-'8cd79ed6-73fd-41c6-beec-60c997f675a4': {}, 
-'ad124a6c-8cce-4936-a054-97a1f67c60b7': {}, 
-'fa24dca4-e68a-4f66-b9ff-7a8aad78a74f': {}, 
-'ae7eff45-5df0-4e7b-bd4e-ec4dc1a79d2d': {}, 
-'e945ec09-f74c-42ed-8ae6-809945df54de': {}}, 
-
-'output_tripleg_level': {
-'cb32d4fe-47fd-4b2f-aefa-01251d2fe3c6': {
-
-'triplegs': ['87cdbd26-ed09-466f-8998-283e4b180736', '71d75826-be62-4aea-9158-d25f3e69fada', '4b8936e0-b132-47a1-97c2-0b619e0d1a94', '3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8', '35895f25-8eda-451b-bcc2-f899884b5d5a'], 
-
-'87cdbd26-ed09-466f-8998-283e4b180736': {'transportation_mode': 'walk'}, 
-'71d75826-be62-4aea-9158-d25f3e69fada': {'transportation_mode': 'bus'}, 
-'4b8936e0-b132-47a1-97c2-0b619e0d1a94': {'transportation_mode': 'walk'}, 
-'3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8': {'transportation_mode': 'bus'}, 
-'35895f25-8eda-451b-bcc2-f899884b5d5a': {'transportation_mode': 'unknown'}}, 
-
-'7e6c9839-d276-4e9a-8a30-30b3ac1557b9': {'triplegs': ['87cdbd26-ed09-466f-8998-283e4b180736', '71d75826-be62-4aea-9158-d25f3e69fada', '4b8936e0-b132-47a1-97c2-0b619e0d1a94', '3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8', '35895f25-8eda-451b-bcc2-f899884b5d5a'], 
-'87cdbd26-ed09-466f-8998-283e4b180736': {'transportation_mode': 'walk'}, 
-'71d75826-be62-4aea-9158-d25f3e69fada': {'transportation_mode': 'bus'}, 
-'4b8936e0-b132-47a1-97c2-0b619e0d1a94': {'transportation_mode': 'walk'}, 
-'3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8': {'transportation_mode': 'bus'}, 
-'35895f25-8eda-451b-bcc2-f899884b5d5a': {'transportation_mode': 'unknown'}}, 
-
-'8cd79ed6-73fd-41c6-beec-60c997f675a4': {'triplegs': ['87cdbd26-ed09-466f-8998-283e4b180736', '71d75826-be62-4aea-9158-d25f3e69fada', '4b8936e0-b132-47a1-97c2-0b619e0d1a94', '3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8', '35895f25-8eda-451b-bcc2-f899884b5d5a'], 
-'87cdbd26-ed09-466f-8998-283e4b180736': {'transportation_mode': 'walk'}, 
-'71d75826-be62-4aea-9158-d25f3e69fada': {'transportation_mode': 'bus'}, 
-'4b8936e0-b132-47a1-97c2-0b619e0d1a94': {'transportation_mode': 'walk'}, 
-'3fd3631d-4cb4-4d0a-aaeb-804ebe7e46f8': {'transportation_mode': 'bus'}, 
-'35895f25-8eda-451b-bcc2-f899884b5d5a': {'transportation_mode': 'unknown'}}, 
-
-'ad124a6c-8cce-4936-a054-97a1f67c60b7': {'triplegs': ['0740ca13-e40b-4500-9329-d44cfd642376', 'd0eb8407-dc8f-4d27-930f-f792030ca8e1', '3fea060e-0dc8-4705-aabb-8e960c644f90'], 
-'0740ca13-e40b-4500-9329-d44cfd642376': {'transportation_mode': 'bus'}, 
-'d0eb8407-dc8f-4d27-930f-f792030ca8e1': {'transportation_mode': 'bus'}, 
-'3fea060e-0dc8-4705-aabb-8e960c644f90': {'transportation_mode': 'cycle'}}, 
-
-'fa24dca4-e68a-4f66-b9ff-7a8aad78a74f': {'triplegs': ['0740ca13-e40b-4500-9329-d44cfd642376', 'd0eb8407-dc8f-4d27-930f-f792030ca8e1', '3fea060e-0dc8-4705-aabb-8e960c644f90'], 
-'0740ca13-e40b-4500-9329-d44cfd642376': {'transportation_mode': 'bus'}, 
-'d0eb8407-dc8f-4d27-930f-f792030ca8e1': {'transportation_mode': 'bus'}, 
-'3fea060e-0dc8-4705-aabb-8e960c644f90': {'transportation_mode': 'cycle'}}, 
-
-'ae7eff45-5df0-4e7b-bd4e-ec4dc1a79d2d': {'triplegs': ['0740ca13-e40b-4500-9329-d44cfd642376', 'd0eb8407-dc8f-4d27-930f-f792030ca8e1', '3fea060e-0dc8-4705-aabb-8e960c644f90'], 
-'0740ca13-e40b-4500-9329-d44cfd642376': {'transportation_mode': 'bus'}, 
-'d0eb8407-dc8f-4d27-930f-f792030ca8e1': {'transportation_mode': 'bus'}, 
-'3fea060e-0dc8-4705-aabb-8e960c644f90': {'transportation_mode': 'cycle'}}, 
-
-'e945ec09-f74c-42ed-8ae6-809945df54de': {'triplegs': ['f7b6d950-dd89-48fd-becc-33d93be8ac77', '96065b79-3d6f-4bd5-ba29-6692b1860926', 'a031c77f-ebd8-44ff-a772-997f62976219'], 
-'f7b6d950-dd89-48fd-becc-33d93be8ac77': {'transportation_mode': 'cycle'}, 
-'96065b79-3d6f-4bd5-ba29-6692b1860926': {'transportation_mode': 'unknown'}, 
-'a031c77f-ebd8-44ff-a772-997f62976219': {'transportation_mode': 'unknown'}}}}
-'''
-
-
 
 ########################################################################################################################
 ########################################################################################################################
